b510d7ef14a0547cb93d755a8b5aeb22aa7e1fd0
hani seid <146775210+hani2628@users.noreply.github.com>
2024-10-07 13:49:44 -0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
                 <p>IT'S TIME TO GET</p>
                 <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Kamil<span className='text-blue-400'>Fitness</span></h1>
             </div>
-            <p className='text-sm md:text-base font-light'><span className='text-blue-400 font-medium'></span><span className='text-blue-400 font-medium'></span></p>
+            <p className='text-sm md:text-base font-light'>shredded<span className='text-blue-400 font-medium'>by clicking accept</span><span className='text-blue-400 font-medium'>i hereby agree to stop breaking chairs and become fit</span></p>
             <Button func={() => {
                 window.location.href = '#generate'
             }} text={"Accept & Begin"}></Button>
